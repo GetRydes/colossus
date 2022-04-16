36ea27cac0c87fc68ccd97334e6f7deaa08af625
Adebayo Onuchukwu <adebayop.o@gmail.com>
2022-04-17 00:34:53 +0100

    feat: setup graphql server with typeorm as ORM and postgres DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { DataAccessModule } from './data-access/data-access.module';
 import { UtilsModule } from './utils/utils.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
-    AuthModule,
     DataAccessModule,
     UtilsModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
