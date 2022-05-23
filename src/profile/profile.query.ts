79aefececf7c74e3d482bba87617e59a25a422a3
Adebayo Onuchukwu <adebayop.o@gmail.com>
2022-05-23 23:25:20 +0100

    feat: driver prodile update

--- src/profile/profile.query.ts
+++ src/profile/profile.query.ts
@@ -1,11 +1,14 @@
 import { Resolver, Query, Args, Int } from '@nestjs/graphql';
 import { ProfileService } from './profile.service';
 import { Driver } from './entities/driver.entity';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard';
 
 @Resolver(() => Driver)
 export class ProfileQueryResolver {
   constructor(private readonly profileService: ProfileService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Driver], { name: 'profiles' })
   async findOneOrAll(
     @Args('id', { type: () => Int, nullable: true }) id?: number,
